e3526724a75c8aee72407d2b2f210c96222437ab
정우혁 <woohyeoks807@naver.com>
2020-10-28 00:55:02 +0900

    [SuperSocket] - ChatServer 작업중

---
+++ SuperSocket/Tutorials/ChatServer/ChatServer/PacketProcessor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks.Dataflow;
+
+namespace ChatServer
+{
+    class PacketProcessor
+    {
+        bool IsThreadRunning = false;
+
+        // receive쪽에서 처리하지 않아도 Post에서 블럭킹 되지 않는다.
+        // BufferBlock<T>(DataflowBlockOptions) 에서 DataflowBlockOptions의 BoundedCapacity로 버퍼 가능 수 지정. BoundedCapacity 보다 크게 쌓이면 블럭킹 된다
+        BufferBlock<ServerPacketData> MsgBuffer = new BufferBlock<ServerPacketData>();
+
+        public void InsertPacket(ServerPacketData data)
+        {
+            MsgBuffer.Post(data);
+        }
+
+        public void Destory()
+        {
+            IsThreadRunning = false;
+            MsgBuffer.Complete();
+        }
+
+    }
+}
---
+++ SuperSocket/Tutorials/ChatServer/ChatServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ChatServer!");
+
+            var serverOption = new ChatServerOption();
+            serverOption.Name = "ChatServer";
+            serverOption.MaxConnectionNumber = 100;
+            serverOption.Port = 8888;
+            serverOption.MaxRequestLength = 1024;
+            serverOption.ReceiveBufferSize = 1024;
+            serverOption.SendBufferSize = 1024;
+
+            var server = new MainServer();
+            server.InitConfig(serverOption);
+            server.CreateStartServer();
+
+            MainServer.MainLogger.Info("Press q to shut down the server");
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(50);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.KeyChar == 'q')
+                    {
+                        Console.WriteLine("Server Terminate ~~~");
+                        server.StopServer();
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+}
